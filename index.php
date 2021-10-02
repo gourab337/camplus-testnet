2050dd7784bda4141a3493480f5ef945de96f491
Gourab Chakraborty <19bcs118@iiitdwd.ac.in>
2021-10-03 00:08:40 +0530

    Added server script

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Venus Template</title>
+    <title>Camp+</title>
+	<link rel = "icon" href = "Branding/Camplus Logo.png">
     <link href="https://fonts.googleapis.com/css?family=Heebo:400,700|Oxygen:700" rel="stylesheet">
     <link rel="stylesheet" href="dist/css/style.css">
     <script src="https://unpkg.com/scrollreveal@4.0.5/dist/scrollreveal.min.js"></script>
